f98e0f230075654482281b55653b1797d0c75afd
kotmin <70173732+Kotmin@users.noreply.github.com>
2024-06-05 00:26:47 +0200

    add morgan and bodyparser

--- server/server.js
+++ server/server.js
@@ -2,22 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+var morgan = require('morgan');
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+app.use(morgan("tiny"))
+app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
 
 const PORT = process.env.PORT || 5050;
 
+const mongoUrl = 'mongodb://localhost:27017/mernapp';
+
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('Connected to MongoDB'))
+// mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(mongoUrl, {})
+  .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.log(error));
+
 
 // Routes
 app.use('/api/articles', require('./routes/articleRoutes'));
