8048e4ffdfe1003f25d28908db234b85aafbf462
kotmin <70173732+Kotmin@users.noreply.github.com>
2024-06-12 13:45:19 +0200

    working theme toggle

--- client/src/components/ThemeToggle.js
+++ client/src/components/ThemeToggle.js
@@ -13,22 +13,46 @@ const ThemeToggle = () => {
     }
   }, [darkMode]);
 
+  useEffect(() => {
+    const darkMode = localStorage.getItem('dark') === 'enabled';
+    setDarkMode(darkMode);
+  }, []);
+
   const handleToggle = () => {
     setDarkMode(prevMode => !prevMode);
   };
 
-  return (
-    <div style={{ position: 'fixed', top: '10px', right: '10px' }}>
-      <label>
-        <input
-          type="checkbox"
-          checked={darkMode}
-          onChange={handleToggle}
-        />
-        {darkMode ? 'Dark Mode' : 'Light Mode'}
+//   return (
+//     <div style={{ position: 'fixed', top: '10px', right: '10px' }}>
+//       <label>
+//         <input
+//           type="checkbox"
+//           checked={darkMode}
+//           onChange={handleToggle}
+//         />
+//         {darkMode ? 'Dark Mode' : 'Light Mode'}
+//       </label>
+//     </div>
+//   );
+
+return (
+    <div className="theme_toggle">
+      <input
+        type="checkbox"
+        className="checkbox_swype"
+        id="night_theme_toggle"
+        checked={darkMode}
+        onChange={handleToggle}
+        aria-hidden="true"
+      />
+      <label htmlFor="night_theme_toggle" className="label_toggle">
+        <i className="sun"></i>
+        <i className="moon"></i>
+        <i className="ball"></i>
       </label>
     </div>
   );
+
 };
 
 export default ThemeToggle;
