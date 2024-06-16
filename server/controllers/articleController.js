b5e519fd0f76892d5a65b40ab921024dbf5f0476
kotmin <70173732+Kotmin@users.noreply.github.com>
2024-06-16 16:22:05 +0200

    error witch fetching articles but detail view is rendered

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -81,36 +81,79 @@ exports.getAllArticles = async (req, res) => {
 //   }
 // };
 
+// exports.getArticleById = async (req, res) => {
+//   try {
+//     const article = await Article.findById(req.params.id).populate('category').populate({
+//       path: 'authors',
+//       select: '_id username profileDescription articles'
+//     });
+//     if (!article) return res.status(404).json({ message: 'Article not found' });
+
+//     // Authorization check
+//     if (req.user) {
+//       if (
+//         req.user.rank === 'Worker' ||
+//         req.user.rank === 'Moderator' ||
+//         req.user.rank === 'Admin' ||
+//         article.authors.some(author => author._id.equals(req.user._id)) ||
+//         article.status === 'published'
+//       ) {
+//         return res.json(article);
+//       } else {
+//         return res.status(403).json({ message: 'You do not have permission to view this article' });
+//       }
+//     } else if (article.status === 'published') {
+//       return res.json(article);
+//     } else {
+//       return res.status(403).json({ message: 'You do not have permission to view this article' });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+
 exports.getArticleById = async (req, res) => {
   try {
-    const article = await Article.findById(req.params.id).populate('category').populate({
-      path: 'authors',
-      select: '_id username profileDescription articles'
-    });
-    if (!article) return res.status(404).json({ message: 'Article not found' });
+    let query = { status: 'published', _id: req.params.id };
 
-    // Authorization check
     if (req.user) {
-      if (
-        req.user.rank === 'Worker' ||
-        req.user.rank === 'Moderator' ||
-        req.user.rank === 'Admin' ||
-        article.authors.some(author => author._id.equals(req.user._id)) ||
-        article.status === 'published'
-      ) {
-        return res.json(article);
-      } else {
-        return res.status(403).json({ message: 'You do not have permission to view this article' });
+      if (req.user.rank === 'Worker' || req.user.rank === 'Moderator' || req.user.rank === 'Admin') {
+        // Worker, Moderator, Admin - all Articles
+        query = { _id: req.params.id };
+      } else if (req.user.rank === 'Regular') {
+        query = {
+          _id: req.params.id,
+          $or: [
+            { authors: req.user.id },
+            { status: 'published' }
+          ]
+        };
       }
-    } else if (article.status === 'published') {
-      return res.json(article);
-    } else {
-      return res.status(403).json({ message: 'You do not have permission to view this article' });
     }
+
+    const article = await Article.findOne(query)
+      .populate('category')
+      .populate({
+        path: 'authors',
+        select: '_id username profileDescription articles',
+        populate: {
+          path: 'articles',
+          select: '_id title'
+        }
+      });
+
+    if (!article) {
+      return res.status(404).json({ message: 'Article not found' });
+    }
+
+    res.json(article);
   } catch (error) {
+    console.error('Error fetching article by ID:', error);
     res.status(500).json({ message: error.message });
   }
 };
+
 
 // Create a new article
 exports.createArticle = async (req, res) => {
